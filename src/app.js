bf5de6492d8c5383e1760f0f1123949abf2f1af8
CodeAaronM <aaron.martinez.b@gmail.com>
2024-07-24 20:18:59 +0000

    0.5

--- src/app.js
+++ src/app.js
@@ -1,11 +1,47 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+let boton = getcard();
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+function getcard() {
+  document.querySelector("#numero").innerHTML = numero();
+  switch (pinta()) {
+    case 1:
+      document.querySelector("#pinta1").innerHTML =
+        '<i class="fa-solid fa-heart"></i>';
+      document.querySelector("#pinta2").innerHTML =
+        '<i class="fa-solid fa-heart"></i>';
+      break;
+    case 2:
+      document.querySelector("#pinta1").innerHTML =
+        '<i class="fa-solid fa-diamond"></i>';
+      document.querySelector("#pinta2").innerHTML =
+        '<i class="fa-solid fa-diamond"></i>';
+      break;
+    case 3:
+      document.querySelector("#pinta1").innerHTML =
+        '<i class="fa-solid fa-clover"></i>';
+      document.querySelector("#pinta2").innerHTML =
+        '<i class="fa-solid fa-clover"></i>';
+      break;
+    case 4:
+      document.querySelector("#pinta1").innerHTML =
+        '<i class="fa-solid fa-rocket"></i>';
+      document.querySelector("#pinta2").innerHTML =
+        '<i class="fa-solid fa-rocket"></i>';
+      break;
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+    default:
+      document.querySelector("#pinta1").innerHTML =
+        '<i class="<i class="fa-solid fa-wrench"></i>';
+      document.querySelector("#pinta2").innerHTML =
+        '<i class="<i class="fa-solid fa-rocket"></i>';
+      break;
+  }
+}
+
+function numero() {
+  return Math.ceil(Math.random() * 13);
+}
+
+function pinta() {
+  return Math.ceil(Math.random() * 4);
+}
+getcard();
